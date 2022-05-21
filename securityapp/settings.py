4891f00b254993c55804b9667838fdfc587a1b4d
Danilotumix <dani96hbo@gmail.com>
2022-05-20 20:17:13 -0500

    Test

--- securityapp/settings.py
+++ securityapp/settings.py
@@ -201,7 +201,9 @@ STATIC_URL = '/static/'
 
 MEDIA_URL = '/media/'
 
-STATICFILES_DIRS = [os.path.join(BASE_DIR, "static")]
+STATICFILES_DIRS = (
+    os.path.join(BASE_DIR, 'static'),
+)
 
 MEDIA_ROOT = os.path.join(BASE_DIR, 'static/media')
 
