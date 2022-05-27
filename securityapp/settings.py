f1f5c1b0615f2fab41ed8484bf5c31dfe7ed1bf9
Danilotumix <dani96hbo@gmail.com>
2022-05-26 23:19:00 -0500

    Test

--- securityapp/settings.py
+++ securityapp/settings.py
@@ -27,7 +27,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'NULL'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ['*']
 
