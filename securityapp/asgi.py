a5383cf33ebd32e6af52e9f252803699824d248e
Danilotumix <dani96hbo@gmail.com>
2022-05-28 16:53:44 -0500

    test

--- securityapp/asgi.py
+++ securityapp/asgi.py
@@ -7,17 +7,18 @@ For more information on this file, see
 https://docs.djangoproject.com/en/3.2/howto/deployment/asgi/
 """
 
-import os
-
+import os, incidents.routing 
 from django.core.asgi import get_asgi_application
+
+django_asgi_app = get_asgi_application()
+
 from channels.routing import ProtocolTypeRouter, URLRouter
 from channels.auth import AuthMiddlewareStack
-import incidents.routing
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'securityapp.settings')
 
 application = ProtocolTypeRouter({
-    'http': get_asgi_application(),
+    'http': django_asgi_app,
     'websocket': AuthMiddlewareStack(
         URLRouter(
             incidents.routing.websocket_urlpatterns
