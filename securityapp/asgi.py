cefb077a58af5a9869e52e4f4116a2e04257bc34
Danilotumix <dani96hbo@gmail.com>
2022-05-28 16:57:29 -0500

    test

--- securityapp/asgi.py
+++ securityapp/asgi.py
@@ -7,11 +7,11 @@ For more information on this file, see
 https://docs.djangoproject.com/en/3.2/howto/deployment/asgi/
 """
 
-import os, incidents.routing 
 from django.core.asgi import get_asgi_application
 
 django_asgi_app = get_asgi_application()
 
+import os, incidents.routing 
 from channels.routing import ProtocolTypeRouter, URLRouter
 from channels.auth import AuthMiddlewareStack
 
