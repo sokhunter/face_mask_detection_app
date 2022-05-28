904a2f0d23768418299859a41bfa6a36cbdef904
Danilotumix <dani96hbo@gmail.com>
2022-05-27 22:09:58 -0500

    Covid data

--- incidents/functions.py
+++ incidents/functions.py
@@ -1,6 +1,7 @@
 import collections
 
-from datetime import datetime
+from datetime import datetime, timedelta
+from dateutil.relativedelta import relativedelta
 from django.utils import timezone
 
 from incidents.models import Incident
@@ -56,3 +57,38 @@ def get_incidents_by_date_range(request, start_date, end_date, camera, category)
         incidents = incidents.filter(security_user=security_user)
 
     return incidents, fstart_date, fend_date
+
+
+def get_period_ranges():
+    date_now = timezone.localtime(timezone.now()).replace(hour=0, minute=0, second=0, microsecond=0)
+
+    day_start = timezone.make_aware(datetime.combine(date_now, datetime.min.time()))
+    day_end = timezone.make_aware(datetime.combine(date_now, datetime.max.time()))
+
+    prev_day_start = timezone.make_aware(datetime.combine(date_now - timedelta(days=1), datetime.min.time()))
+    prev_day_end = timezone.make_aware(datetime.combine(date_now - timedelta(days=1), datetime.max.time()))
+
+    week_start = timezone.make_aware(datetime.combine(date_now - timedelta(days=date_now.weekday()), datetime.min.time()))
+    week_end = day_end
+
+    prev_week_start = timezone.make_aware(datetime.combine(week_start - timedelta(days=7), datetime.min.time()))
+    prev_week_end = timezone.make_aware(datetime.combine(week_end - timedelta(days=7), datetime.max.time()))
+
+    month_start = timezone.make_aware(datetime.combine(date_now.replace(day=1), datetime.min.time()))
+    month_end = day_end
+    
+    prev_month_start = timezone.make_aware(datetime.combine((month_start - timedelta(days=1)).replace(day=1), datetime.min.time()))
+
+    prev_month_end = prev_month_start.replace(day=28) + timedelta(days=4)
+    prev_month_end = timezone.make_aware(datetime.combine(prev_month_end - timedelta(days=prev_month_end.day), datetime.max.time()))
+
+    if month_end.day < prev_month_end.day:
+        prev_month_end = prev_month_end.replace(day=month_end.day)
+
+    year_start = timezone.make_aware(datetime.combine(date_now.replace(day=1,month=1), datetime.min.time()))
+    year_end = day_end
+
+    prev_year_start = timezone.make_aware(datetime.combine(date_now.replace(day=1,month=1,year=date_now.year-1), datetime.min.time()))
+    prev_year_end = timezone.make_aware(datetime.combine(date_now - relativedelta(years=1), datetime.max.time()))
+
+    return day_start, day_end, prev_day_start, prev_day_end, week_start, week_end, prev_week_start, prev_week_end, month_start, month_end, prev_month_start, prev_month_end, year_start, year_end, prev_year_start, prev_year_end
