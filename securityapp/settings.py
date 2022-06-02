199b5a937c64ca22e60f7701241974667dd9d82e
Josue Cuentas Jave <55953542+DarkMage17@users.noreply.github.com>
2022-06-01 20:24:42 -0500

    Hotfix show recommendations

--- securityapp/settings.py
+++ securityapp/settings.py
@@ -129,15 +129,9 @@ DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql_psycopg2',
 
-        'NAME': 'securityDB',
-        'USER': 'postgres',
-        'PASSWORD': '1234',
-       # 'NAME': 'securityDb',
-       # 'USER': 'josue',
-       # 'PASSWORD': 'cuentas',
-       # 'NAME': 'securityDb',
-       # 'USER': 'josue',
-       # 'PASSWORD': 'cuentas',
+        'NAME': 'securityDb',
+        'USER': 'josue',
+        'PASSWORD': 'cuentas',
 
         'HOST': '127.0.0.1',
         'PORT': '5432',
